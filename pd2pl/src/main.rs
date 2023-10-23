4cf2e57d5a54d0dd7773c243c5c93a0e80bdc663
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-22 21:05:50 -0600

    parse arguments

--- pd2pl/src/main.rs
+++ pd2pl/src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::process;
+use pd2pl::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
+
+    println!("file path {}", config.file_path);
+    println!("output path {}", config.output_path);
 }
