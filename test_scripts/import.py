719769468440d87ae657eaa7cb9a2cecaa19cea4
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-23 13:45:41 -0600

    add pandas function calls to test python script

--- test_scripts/import.py
+++ test_scripts/import.py
@@ -1,2 +1,7 @@
 import numpy as np
 import pandas as pd
+
+PATH = './awesome/path.parquet'
+df = pd.read_csv(PATH, dtypes={'col_1': 'float', 'col_2': 'str'})
+
+df['time'] = pd.to_datetime(df['col_2'])
